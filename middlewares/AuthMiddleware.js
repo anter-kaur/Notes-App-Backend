64a61d8ba22b9ef2a0e84fcd8622ee71889904e8
anter-kaur <anterpreet905@gmail.com>
2024-05-07 16:40:23 +0530

    token debugged

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -2,9 +2,20 @@ import jwt from 'jsonwebtoken'
 
 const AuthMiddleware=(req,res,next)=>{
     try{
-        const token=req.headers["authorization"] || req.cookies.accessToken;
+    //     const token=req.headers["authorization"] || req.cookies.accessToken;
         
-    const decodedToken=jwt.verify(token,process.env.JWT_ACCESS_TOKEN)
+    // const decodedToken=jwt.verify(token,process.env.JWT_ACCESS_TOKEN)
+
+    let token = null;
+
+        // Check if the Authorization header exists and if it starts with "Bearer "
+        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+            // Extract the token by removing the "Bearer " prefix
+            token = req.headers.authorization.split(" ")[1];
+        } else if (req.cookies.accessToken) {
+            // If the Authorization header is not present, check for accessToken in cookies
+            token = req.cookies.accessToken;
+        }
 
     // if(!decodedToken){
     //     res.status(400).json({message:"Invalid Token"})
